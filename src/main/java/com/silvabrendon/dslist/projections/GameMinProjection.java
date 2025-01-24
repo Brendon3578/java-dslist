550ef746c4a2c12d26e95e751004588373f4ed7e
Brendon Gomes <brendongomes40@gmail.com>
2025-01-24 09:42:39 -0300

    create application properties and fix query bug for postgresql

--- src/main/java/com/silvabrendon/dslist/projections/GameMinProjection.java
+++ src/main/java/com/silvabrendon/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.silvabrendon.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
